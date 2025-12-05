b4f6d14738461ef95aed49f9899b689ea0fcb586
应钟火鼠丶 <zhuyifa1028@gmail.com>
2025-12-05 15:39:10 +0800

    POM管理

---
+++ ruoyi-module/ruoyi-module-system/src/main/java/org/dromara/system/mapper/SysClientMapper.java
@@ -0,0 +1,15 @@
+package org.dromara.system.mapper;
+
+import org.dromara.common.mybatis.core.mapper.BaseMapperPlus;
+import org.dromara.system.domain.SysClient;
+import org.dromara.system.domain.vo.SysClientVo;
+
+/**
+ * 授权管理Mapper接口
+ *
+ * @author Michelle.Chung
+ * @date 2023-05-15
+ */
+public interface SysClientMapper extends BaseMapperPlus<SysClient, SysClientVo> {
+
+}
